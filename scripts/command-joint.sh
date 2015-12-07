aff69d565de2a2943145f64febe7d7b31f2b8de1
zhangmeishan <mason.zms@gmail.com>
2015-12-07 10:01:48 +0800

    Update command-joint.sh
    Update joint

--- scripts/command-joint.sh
+++ scripts/command-joint.sh
@@ -1,14 +1,10 @@
 
 for i in `seq 1 10`;
 do
-	echo "MultiLabelerBasic $i "		
-	./MultiLabelerBasic -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jb.crf  -word emb/word2vec.tin.emb
-	echo "MultiLabelerNewBasic $i"
- 	./MultiLabelerNewBasic -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jnb.crf -word emb/word2vec.tin.emb 
-	echo "MultiLabelerTanh $i"	     
-	./MultiLabelerTanh -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jlt.crf -word emb/word2vec.tin.emb 
-	echo "MultiLabelerNewTanh $i"
-	./MultiLabelerNewTanh -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jnlt.crf -word emb/word2vec.tin.emb 
-	echo "MultiLabelerNNTanh $i"
-	./MultiLabelerNNTanh -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jt.crf -word emb/word2vec.tin.emb 
+	echo "MultiDiscreteCRFMMLabeler $i "		
+	./MultiDiscreteCRFMMLabeler -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jb.crf  -word emb/word2vec.tin.emb
+	echo "MultiDenseCRFMMLabeler $i"
+ 	./MultiDenseCRFMMLabeler -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jnb.crf -word emb/word2vec.tin.emb 
+	echo "MultiDcombCRFMMLabeler  $i"	     
+	./MultiDcombCRFMMLabeler  -l -train joint/train$i.nn -dev joint/dev$i.nn -test joint/test$i.nn -model test.model -option option.jlt.crf -word emb/word2vec.tin.emb 
 done
